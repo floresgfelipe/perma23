6712b6726c2620c65948ee335e0f031d4bb5995c
Felipe Flores <floresgfelipealberto@outlook.com>
2023-08-16 19:17:47 -0600

    participante shell

--- perma23.py
+++ perma23.py
@@ -1,10 +1,11 @@
 from app import app, db
-from app.models import Alumno
+from app.models import Alumno, Participante
 
 @app.shell_context_processor
 def make_shell_context():
     return {
         'db': db, 
-        'Alumno': Alumno, 
+        'Alumno': Alumno,
+        'Participante': Participante, 
     }
     
